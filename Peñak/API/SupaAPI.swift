a03a9980ce5b77acd68d982a1b38ea93d5dc1d1f
DrikABrak <ctrespeuch@gmail.com>
2024-09-26 11:23:15 +0200

    Clean code

--- Peñak/API/SupaAPI.swift
+++ Peñak/API/SupaAPI.swift
@@ -19,29 +19,6 @@ public final class SupaAPI {
     
     public init() {}
 
-    let client: SupabaseClient = SupabaseClient(supabaseURL: URL(string: "https://ipzabqlvaayfafvkgrde.supabase.co")!,
-                                                supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwemFicWx2YWF5ZmFmdmtncmRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ0MjAwMTYsImV4cCI6MjAzOTk5NjAxNn0.pKpGnNhR3DH9OQ8SXSDE9A_ghcdbgfPoxrfma5jxcjM")
-
-    
-//    func request<T: Decodable>(type: EndPointType) async throws -> T {
-//        guard let url = type.url else { throw NetworkError.invalidURL }
-//
-//        var request = URLRequest(url: url)
-//        request.httpMethod = type.method.rawValue
-//
-//        if let body = type.body {
-//            request.httpBody = try? JSONEncoder().encode(body)
-//        }
-//
-//        request.allHTTPHeaderFields = type.headers
-//        let (data, response) = try await URLSession.shared.data(for: request)
-//
-//        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw NetworkError.invalidResponse }
-//
-//        return try JSONDecoder().decode(T.self, from: data)
-//    }
-//
-//    static var commonHeaders: [String: String]? {
-//        ["accept" : "application/json"]
-//    }
+    let client: SupabaseClient = SupabaseClient(supabaseURL: URL(string: "URL_SUPABASE")!,
+                                                supabaseKey: "KEY_SUPABASE")
 }
--- Peñak/API/SupaService.swift
+++ Peñak/API/SupaService.swift
@@ -28,5 +28,3 @@ class SupaService {
         }
     }
 }
-
-//import Observation
